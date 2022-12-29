268885a61b75c6de1f9cfb906765fe2f32b30286
Leonardo França <lsfranca@revizia.com.br>
2022-12-29 17:04:51 -0300

    Commit Inicial

---
+++ src/main/java/com/example/crudspringjava/CrudSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudspringjava/CrudSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudspringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
