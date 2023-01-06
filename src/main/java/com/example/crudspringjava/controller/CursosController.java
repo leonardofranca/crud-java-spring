2232bfaa859c9f99973fbb871231f2740f0f7cb1
Leonardo França <lsfranca@revizia.com.br>
2023-01-06 14:22:32 -0300

    Implementando endpoint getById

--- src/main/java/com/example/crudspringjava/controller/CursosController.java
+++ src/main/java/com/example/crudspringjava/controller/CursosController.java
@@ -3,9 +3,11 @@ package com.example.crudspringjava.controller;
 import com.example.crudspringjava.model.Curso;
 import com.example.crudspringjava.repository.CursoRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cursos")
@@ -15,6 +17,12 @@ public class CursosController {
 
     public CursosController(CursoRepository cursoRepository) {
         this.cursoRepository = cursoRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Curso> list(@PathVariable Long id) {
+        return cursoRepository.findById(id).map(curso -> ResponseEntity.status(HttpStatus.OK).body(curso))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping
