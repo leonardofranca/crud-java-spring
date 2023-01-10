80959832bde726995d5fd5b0a14a6fca27a46d1a
Leonardo França <lsfranca@revizia.com.br>
2023-01-10 11:01:29 -0300

    Adicionando soft delete

--- src/main/java/com/example/crudspringjava/model/Curso.java
+++ src/main/java/com/example/crudspringjava/model/Curso.java
@@ -6,11 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
 //@Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Curso SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Curso {
 
     @Id
@@ -29,4 +33,11 @@ public class Curso {
     @Pattern(regexp = "Back|Front")
     @Column(length = 10, nullable = false)
     private String categoria;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
+
 }
